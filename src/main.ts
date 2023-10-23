d606f6f0efb62c702116b0b1cd007e54230e3220
Stephan Strittmatter <stritti@users.noreply.github.com>
2023-10-23 13:05:51 +0000

    migrate to scss

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
-import './assets/main.css'
+import './assets/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 const app = createApp(App)
 
