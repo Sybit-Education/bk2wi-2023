09997e01a8230c10c5189a71d536e267e7727adb
Lenny11y <148078971+Lenny11y@users.noreply.github.com>
2023-10-30 14:49:41 +0000

    6

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import type LocaleeHeaderVue from './components/LocaleeHeader.vue';
+
 
 
 </script>
 
 <template>
-  <LocaleeHeaderVue/>
+  
   <RouterView/>
 </template>
 
--- src/components/LocaleeHeader.vue
+++ src/components/LocaleeHeader.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+
 </script>
+
 
 
 <template>
