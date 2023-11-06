70ce44047e5230bbaafde06c0be1a57c67e9e389
Stephan Strittmatter <stritti@users.noreply.github.com>
2023-11-06 13:32:03 +0000

    chore: missing dept for airtable added

--- components.d.ts
+++ components.d.ts
@@ -7,29 +7,14 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    BButton: typeof import('bootstrap-vue-next')['BButton']
     BCard: typeof import('bootstrap-vue-next')['BCard']
     BCardGroup: typeof import('bootstrap-vue-next')['BCardGroup']
     BCardText: typeof import('bootstrap-vue-next')['BCardText']
     BCardTitle: typeof import('bootstrap-vue-next')['BCardTitle']
-    BCol: typeof import('bootstrap-vue-next')['BCol']
-    BCollapse: typeof import('bootstrap-vue-next')['BCollapse']
-    BContainer: typeof import('bootstrap-vue-next')['BContainer']
-    BDropdownItem: typeof import('bootstrap-vue-next')['BDropdownItem']
-    BFormInput: typeof import('bootstrap-vue-next')['BFormInput']
-    BNavbar: typeof import('bootstrap-vue-next')['BNavbar']
-    BNavbarBrand: typeof import('bootstrap-vue-next')['BNavbarBrand']
-    BNavbarNav: typeof import('bootstrap-vue-next')['BNavbarNav']
-    BNavbarToggle: typeof import('bootstrap-vue-next')['BNavbarToggle']
-    BNavForm: typeof import('bootstrap-vue-next')['BNavForm']
-    BNavItem: typeof import('bootstrap-vue-next')['BNavItem']
-    BNavItemDropdown: typeof import('bootstrap-vue-next')['BNavItemDropdown']
-    BRow: typeof import('bootstrap-vue-next')['BRow']
     ContentItem: typeof import('./src/components/ContentItem.vue')['default']
     EventItem: typeof import('./src/components/events/EventItem.vue')['default']
     EventList: typeof import('./src/components/events/EventList.vue')['default']
     FilterSidebar: typeof import('./src/components/FilterSidebar.vue')['default']
-    FooterBar: typeof import('./src/components/FooterBar.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     HomeContent: typeof import('./src/components/HomeContent.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
@@ -38,15 +23,11 @@ declare module 'vue' {
     IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
     IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
     LocaleeHeader: typeof import('./src/components/LocaleeHeader.vue')['default']
-    NavigationBar: typeof import('./src/components/NavigationBar.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SearchBar: typeof import('./src/components/SearchBar.vue')['default']
     Template: typeof import('./src/components/template.vue')['default']
     TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
     WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
-  }
-  export interface ComponentCustomProperties {
-    vBColorMode: typeof import('bootstrap-vue-next')['vBColorMode']
   }
 }
