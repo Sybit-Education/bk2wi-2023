6ee31ff3c3f09899d6a439b1a78d595807dbf696
Stephan Strittmatter <stritti@users.noreply.github.com>
2023-11-13 15:05:49 +0000

    chore: add loading indicator

--- src/components/events/EventList.vue
+++ src/components/events/EventList.vue
@@ -1,15 +1,23 @@
 <template>
     <b-card-group columns="true" class="my-3">
-     <event-item
-            v-for="event in eventList"
-            :key="event.id"
-            :event="event"
-          />
+      <BPlaceholderWrapper  :loading="showLoadingSpinner">
+        <template #loading>
+          <BPlaceholderCard style="min-width: 10rem;" no-footer />
+          <BPlaceholderCard style="min-width: 10rem;" no-footer />
+          <BPlaceholderCard style="min-width: 10rem;" no-footer />
+        </template>    
+        <event-item
+                v-for="event in eventList"
+                :key="event.id"
+                :event="event"
+              />
+      </BPlaceholderWrapper>
     </b-card-group>
 </template>
 <script lang="ts">
 import type EventItem from '@/models/EventItem';
 import { useEventItemStore } from '@/stores/eventItem';
+import { useLoadingStore } from '@/stores/loading';
 import { mapState } from 'pinia';
 import { defineComponent } from 'vue';
 
@@ -19,6 +27,9 @@ export default defineComponent({
     ...mapState(useEventItemStore, {
       eventList: (store) => store.eventItemList as Array<EventItem>,
     }),
+    ...mapState(useLoadingStore, {
+      showLoadingSpinner: (store) => store.showLoadingSpinner
+    })
   }
 })
 </script>
