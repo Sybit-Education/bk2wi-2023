0176063734dda3e7a139b76489108949b42439fd
Lazar73a <148079148+Lazar73a@users.noreply.github.com>
2023-11-20 15:08:51 +0000

    ClubList erstellen

---
+++ src/views/ClubListView.vue
@@ -0,0 +1,7 @@
+<script setup ="ts>
+import ClubContent from '@/components/ClubContent.vue'
+import FilterSidebar from '@/components/FilterSidebar.vue'
+import SearchBar from '@/components/SearchBar.vue'
+import type { BRow } from 'bootstrap-vue-next'
+import { defineComponent, ref } from 'vue'
+import Vue from 'vue'
