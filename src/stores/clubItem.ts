2acd770daae3527ebfe42a231a44be3ad090a534
Stephan Strittmatter <stritti@users.noreply.github.com>
2023-11-27 13:48:05 +0000

    chore: Club Detail View added

--- src/stores/clubItem.ts
+++ src/stores/clubItem.ts
@@ -16,6 +16,14 @@ export const useClubItemStore = defineStore('club', {
     getAll: (state) => state.clubItemList,
     getById: (state) => (id: string) =>
       state.clubItemList.find((club: ClubItem) => club.id === id),
+    imageById: (state) => (id: string) => {
+        const item = state.clubItemList.find((club: ClubItem) => club.id === id)
+        if (item && item.bild) {
+            return item.bild[0].thumbnails.large.url
+        } else {
+            return ''
+        }
+    }
   },
   actions: {
     async load() {
