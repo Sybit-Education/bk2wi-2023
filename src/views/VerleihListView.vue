ba020b49ff577528e4348eb2d22532772777c955
Stephan Strittmatter <stritti@users.noreply.github.com>
2023-12-13 14:27:43 +0000

    chore: list of Verleih

--- src/views/VerleihListView.vue
+++ src/views/VerleihListView.vue
@@ -1,34 +1,17 @@
 <script setup lang="ts">
-// import Verleihlistcontent from '@/components/Verleihlistcontent.vue'
-import FilterSidebar from '@/components/FilterSidebar.vue'
-import SearchBar from '@/components/SearchBar.vue'
-import type { BRow } from 'bootstrap-vue-next'
-import {  ref } from 'vue'
-
-
-const searchQuery = ref('')
-const entries = ref([ "test", '1234', '567'])
+import VerleihList from '../components/verleih/VerleihList.vue'
 
 </script>
 
 <template>
 
-<BContainer>
-  <BRow>
-    <BCol lg = "4">
-      <FilterSidebar />
-    </BCol >
-      <BCol lg = "4">
-        <SearchBar :searchQuery="searchQuery" @searchQuery="searchQuery = $event" />
-        
-    </BCol>
-    <BCol lg = "4">
-      icon
-    </BCol >
-  </BRow>
-  <BRow lg = "12">
-<HomeContent :entries="entries" :foundEntry="searchQuery" />
-  </BRow>
-</BContainer>
+  <div id="verleihList">
+    <BBreadcrumb>
+      <BBreadcrumbItem to="/"> Home </BBreadcrumbItem>
+      <BBreadcrumbItem to="/verleih"> Verleihe </BBreadcrumbItem>
+    </BBreadcrumb>
+    
+    <verleih-list />
+  </div>
 
 </template>
