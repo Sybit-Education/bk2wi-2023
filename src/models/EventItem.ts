2102c2282fb5832f4215d8600bc078c095cf9568
Elijah-Haeb <148079280+Elijah-Haeb@users.noreply.github.com>
2024-01-08 14:40:25 +0000

    Events (event.service & EventItem) erstellen / bearbeiten, verbindung mit Airtable.

--- src/models/EventItem.ts
+++ src/models/EventItem.ts
@@ -1,4 +1,12 @@
 export default interface EventItem {
     id: string;
     name: string;
+    bild: string;
+    beschreinung: string;
+    adresse: string;
+    ort: string;
+    plz: string;
+    datum: string;
+    uhrzeit: string;
+    eintrittspreis: string;
 }
